7a3a518e05210779eef16c1a2ae09acc87880697
Chris Risner <github@chrisrisner.com>
2013-01-21 14:35:46 -0800

    updates

---
+++ source/start/TicTabToeLeaderboard/TicTabToeLeaderboard/LeaderboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  LeaderboardViewController.h
+//  TicTabToeLeaderboard
+//
+//  Created by Chris Risner on 1/21/13.
+//  Copyright (c) 2013 Microsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LeaderboardViewController : UITableViewController
+
+@end
